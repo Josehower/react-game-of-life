69bbda1a2be6b4b3a324791feb96f4cf1dbb6bd2
josehower <josehowerupn@gmail.com>
2021-03-01 22:22:47 +0100

    Add on Click functionality

--- src/App.js
+++ src/App.js
@@ -1,71 +1,75 @@
 /** @jsxImportSource @emotion/core */
-import { css } from '@emotion/core';
-import { useEffect, useState } from 'react';
-import { create2DArray, runTurn } from './utils/createGame';
+import { css, Global } from "@emotion/core";
+import { useEffect, useState } from "react";
+import { create2DArray, runTurn } from "./utils/createGame";
+
+const globalStyles = css`
+body{background-color: #333} 
+`
 
 const columnStyles = css`
   height: 10px;
   padding: 0;
-  margin: 0;
+  margin: 2px;
+  cursor: pointer;
+
+  span {
+    display: inline-block;
+    border: solid 1px #00559b;
+    width: 10px;
+    height: 10px;
+    padding: 0;
+    margin: 0;
+  }
 `;
+
 const live = css`
   display: inline-block;
   border: solid 1px transparent;
   border-radius: 50%;
   width: 10px;
   height: 10px;
-  background-color: blue;
-  padding: 0;
-  margin: 0;
-`;
-const dead = css`
-  display: inline-block;
-  border: solid 1px;
-  width: 10px;
-  height: 10px;
-  background-color: transparent;
+  background-color: #eed966;
   padding: 0;
   margin: 0;
 `;
 
 function App() {
-  const gameGridCopy = create2DArray(15, 15).map((xArr) => xArr.map(() => 0));
-
-  gameGridCopy[5][10 - 9] = 1;
-  gameGridCopy[5][11 - 9] = 1;
-  gameGridCopy[5][12 - 9] = 1;
-  gameGridCopy[4][12 - 9] = 1;
-  gameGridCopy[3][11 - 9] = 1;
-
-  gameGridCopy[10][10 - 9] = 1;
-  gameGridCopy[10][11 - 9] = 1;
-  gameGridCopy[10][12 - 9] = 1;
-  gameGridCopy[9][12 - 9] = 1;
-  gameGridCopy[8][11 - 9] = 1;
+  const gameGridCopy = create2DArray(50, 50).map((xArr) => xArr.map(() => 0));
 
   const [gameGrid, setGameGrid] = useState(gameGridCopy);
   const [isActive, setIsActive] = useState(false);
+
+  function handleClick(x, y) {
+    if (isActive) return;
+    const gameGridCopy = gameGrid.map((column) => [...column]);
+    gameGridCopy[x][y] = gameGridCopy[x][y] ? 0 : 1;
+    setGameGrid(gameGridCopy);
+  }
 
   useEffect(() => {
     const run = setInterval(() => {
       if (isActive) {
         setGameGrid(runTurn(gameGrid));
       }
-    }, 0);
+    }, 100);
     return () => clearInterval(run);
   }, [gameGrid, isActive]);
 
   return (
     <div className="App">
-      {gameGrid.map((column) => {
+      <Global styles={globalStyles} />
+      {gameGrid.map((column, xIndex) => {
         return (
           <div css={columnStyles}>
-            {column.map((cell) =>
+            {column.map((cell, yIndex) =>
               cell === 0 ? (
-                <span css={dead}> </span>
+                <span onClick={() => handleClick(xIndex, yIndex)}> </span>
               ) : (
-                <span css={live}> </span>
-              ),
+                <span css={live} onClick={() => handleClick(xIndex, yIndex)}>
+                  {" "}
+                </span>
+              )
             )}
           </div>
         );
@@ -76,8 +80,9 @@ function App() {
         `}
         onClick={() => setIsActive(!isActive)}
       >
-        {isActive ? 'turn off' : 'turn on'}
+        {isActive ? "turn off" : "turn on"}
       </button>
+    {/* </Global> */}
     </div>
   );
 }
